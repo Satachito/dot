4797b654ec85332f98d90c70faf430c679dbb080
sat <satoru.ogura@me.com>
2022-04-25 18:13:13 +0900

    O3

--- _.sh
+++ _.sh
@@ -1,30 +1,30 @@
 echo "LEGACY double"
-nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_LEGACY -DFLOAT_TYPE=double _.cu
+nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_LEGACY -DFLOAT_TYPE=double -O3 _.cu
 ./a.out
 echo
 
 echo "MANAGED double"
-nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_MANAGED -DFLOAT_TYPE=double _.cu
+nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_MANAGED -DFLOAT_TYPE=double -O3 _.cu
 ./a.out
 echo
 
 echo "LEGACY float"
-nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_LEGACY -DFLOAT_TYPE=float _.cu
+nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_LEGACY -DFLOAT_TYPE=float -O3 _.cu
 ./a.out
 echo
 
 echo "MANAGED float"
-nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_MANAGED -DFLOAT_TYPE=float _.cu
+nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_MANAGED -DFLOAT_TYPE=float -O3 _.cu
 ./a.out
 echo
 
 echo "LEGACY half"
-nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_LEGACY -DFLOAT_TYPE=half _.cu
+nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_LEGACY -DFLOAT_TYPE=half -O3 _.cu
 ./a.out
 echo
 
 echo "MANAGED half"
-nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_MANAGED -DFLOAT_TYPE=half _.cu
+nvcc -arch=sm_86 -lcublas -lcurand -DCUDA_MANAGED -DFLOAT_TYPE=half -O3 _.cu
 ./a.out
 echo
 
